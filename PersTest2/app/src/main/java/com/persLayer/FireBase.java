14458c365aeceb8e8cce9ec32ced0126fbf4a157
Elad Feldman <eladfeld@post.bgu.ac.il>
2020-09-20 17:14:37 +0300

    init

---
+++ PersTest2/app/src/main/java/com/persLayer/FireBase.java
@@ -0,0 +1,62 @@
+package com.persLayer;
+
+import android.media.Image;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FireBase {
+    private boolean StoreImage(String file_path){
+
+        StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
+
+
+        Uri file = Uri.fromFile(new File(file_path);
+        StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
+
+        riversRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl().getResult();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+        return false;
+    }
+
+    private Image getImage() throws IOException {
+        File localFile = File.createTempFile("images", "jpg");
+        riversRef.getFile(localFile)
+                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        // Successfully downloaded data to local file
+                        // ...
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle failed download
+                // ...
+            }
+        });
+    }
+    
+}
---
+++ PersTest2/app/src/main/java/com/persLayer/Repository.java
@@ -0,0 +1,4 @@
+package com.persLayer;
+
+public class Repository {
+}
